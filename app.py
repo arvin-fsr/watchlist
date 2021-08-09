e30f8ea8158a7560eae04904e54b771b4b71b413
arvin <326928338@qq.com>
2021-08-09 13:31:19 +0800

    ok

--- app.py
+++ app.py
@@ -7,9 +7,14 @@ from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 from flask_login import LoginManager, login_user, UserMixin, login_required, logout_user, current_user
 app = Flask(__name__)
 # 写入了一个 SQLALCHEMY_DATABASE_URI 变量来告诉 SQLAlchemy 数据库连接地址;如果是Win系统，使用三个斜线,否则四个斜线
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
-app.config['SECRET_KEY'] = 'dev'  # 等同于 app.secret_key = 'dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')  # 等同于 app.secret_key = 'dev'
 db = SQLAlchemy(app)  # 初始化扩展，传入程序实例 app
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
